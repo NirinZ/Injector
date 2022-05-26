3a6804f93677b603d31a514e347a99731893af6c
Nirin <znirin+gitkraken@gmail.com>
2022-05-26 23:47:01 +0300

    Working version

--- src/Layer 3/.filepart/del.py
+++ src/Layer 3/.filepart/del.py
@@ -1,4 +1,7 @@
+# %%
 import Encoder as enc
 
-di = enc.get_file_checksum(open(r"C:\Users\zniri\Desktop\Coding\Languages\Python\Python Projects\Injector\t.txt",'rb'))
-print(enc.checksum_to_bytes(di).hex())
+di = enc.get_file_checksum(open(r"C:\Users\zniri\Desktop\Coding\Languages\Python\Python Projects\Injector\tm.txt",'rb'))
+print(di)
+by = enc.checksum_to_bytes(di)
+#print(by.hex())
